632bfbc35780f534c223cc2cc1604191fc43258b
omri.weinstein <omri.weinstein@gmail.com>
2023-10-07 17:10:02 +0300

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,21 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=83e54171171bac0dc4c36ba56b30e838&query=' + latitude + ',' + longitude + '&units=f'
+    request( {url, json: true}, (error, {body}) => {
+        if(error) {
+            callback('Unable to connect to location service', undefined)
+        } else if(!body.current){
+            callback('Unable to find location.', undefined)
+        }else{
+            callback(undefined, {
+                temperature: body.current.temperature,
+                feelslike: body.current.feelslike,
+                weather_descriptions: body.current.weather_descriptions[0]
+            })
+        }
+    })
+}
+
+
+module.exports = forecast
